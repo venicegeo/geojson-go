ce7c4240d50c5f1f2256aeb0e351b17be084efb9
Jeff Yutzler <jyutzler@yahoo.com>
2016-10-07 13:19:19 -0400

    Adding WFS support

---
+++ geojson/wfs.go
@@ -0,0 +1,72 @@
+/*
+Copyright 2016, RadiantBlue Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package geojson
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+
+	"github.com/venicegeo/pzsvc-lib"
+)
+
+// FromWFS returns a Feature Collection from the WFS provided.
+// This is a convenience method only and not intended for serious processing.
+// This function does not currently support WFS layers with large numbers of features.
+func FromWFS(wfsURL, featureType string) (*FeatureCollection, error) {
+	var (
+		result   *FeatureCollection
+		err      error
+		request  *http.Request
+		response *http.Response
+		b        []byte
+		ok       bool
+	)
+
+	v := url.Values{}
+	v.Set("service", "wfs")
+	v.Set("count", "9999")
+	v.Set("outputFormat", "application/json")
+	v.Set("version", "2.0.0")
+	v.Set("request", "GetFeature")
+	v.Set("typeName", featureType)
+
+	qurl := wfsURL + "?" + v.Encode()
+
+	if request, err = http.NewRequest("GET", qurl, nil); err != nil {
+		return result, err
+	}
+	if response, err = pzsvc.HTTPClient().Do(request); err != nil {
+		return result, err
+	}
+
+	defer response.Body.Close()
+	b, _ = ioutil.ReadAll(response.Body)
+
+	// Check for HTTP errors
+	if response.StatusCode < 200 || response.StatusCode > 299 {
+		message := fmt.Sprintf("Received %v: \"%v\" when performing a GetFeature request on %v\n%v", response.StatusCode, response.Status, qurl, string(b))
+		return result, &pzsvc.HTTPError{Status: response.StatusCode, Message: message}
+	}
+
+	gjIfc, _ := Parse(b)
+	if result, ok = gjIfc.(*FeatureCollection); ok {
+		return result, nil
+	}
+	return result, pzsvc.ErrWithTrace(fmt.Sprintf("Did not receive valid GeoJSON on request %v", qurl))
+}
---
+++ wfs_test.go
@@ -0,0 +1,46 @@
+/*
+Copyright 2016, RadiantBlue Technologies, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"testing"
+
+	"github.com/venicegeo/geojson-go/geojson"
+)
+
+// TestGeoJSON tests GeoJSON readers
+func TestWFS(t *testing.T) {
+	var (
+		wfsURL      string
+		featureType string
+		fc          *geojson.FeatureCollection
+		err         error
+	)
+	wfsURL = "http://foo.com/wfs"
+	featureType = "bar"
+	if fc, err = geojson.FromWFS(wfsURL, featureType); err == nil {
+		t.Error("This should have returned an error.")
+	}
+	wfsURL = "http://gsp-geose-LoadBala-4EP8UFUE9SXL-919040015.us-east-1.elb.amazonaws.com:80/geoserver/piazza/wfs"
+	featureType = "piazza:8e31e022-4e1f-4a32-b341-4eb019ab45bc"
+	if fc, err = geojson.FromWFS(wfsURL, featureType); err != nil {
+		t.Fatalf("Failed to load WFS layer: %v", err.Error())
+	}
+	if len(fc.Features) == 0 {
+		t.Error("Failed to create any features")
+	}
+}
