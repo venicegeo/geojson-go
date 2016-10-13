457f1ce8201f03a522495bbbd7a67b5234287062
bpeizer <Peizer@Bens-MacBook-Pro.local>
2016-10-13 14:17:40 -0400

    Undid executable change

--- geojson/feature_test.go
+++ geojson/feature_test.go
@@ -83,17 +83,8 @@ func TestFeature(t *testing.T) {
 	}
 	featureCollection := gj.(*FeatureCollection)
 	featureCollection.FillProperties()
-	//featureCollectionB4 := featureCollection.String()
 	fcMap := featureCollection.Map()
 	featureCollection = FeatureCollectionFromMap(fcMap)
-	/*Per Jeff_Y he will need to make changes to FeatureCollections
-	if strings.Compare(featureCollectionB4, featureCollection.String()) != 0 {
-		t.Errorf("featureCollection.geojson failed FillProperties Test")
-		t.Log(featureCollectionB4)
-		t.Log(featureCollection.String())
-		t.Log(fcMap)
-	}*/
-
 	/*Test FeatureFromMap*/
 	if gj, err = ParseFile("test/feature.geojson"); err != nil {
 		t.Errorf("Failed to parse file: %v", err)
