6e163c1e0f7948355597c7782b34aa7241d1d09f
Jeff Yutzler <jyutzler@yahoo.com>
2017-01-25 15:15:00 -0500

    adding aggregate coverage, removing metalinter

--- ci/archive.sh
+++ ci/archive.sh
@@ -22,31 +22,33 @@ go get -v github.com/venicegeo/geojson-go/...
 
 # run unit tests w/ coverage collection
 go test -v -coverprofile=$root/geojson-go.cov github.com/venicegeo/geojson-go/geojson
+go tool cover -func=$root/geojson-go.cov -o $root/geojson-go.cov.txt
 
 # lint
 cd $GOPATH/src/github.com/venicegeo/geojson-go
 
-gometalinter \
---deadline=60s \
---concurrency=6 \
---vendor \
---exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
---exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
---exclude="Api.* should be .*API" \
---exclude="Http.* should be .*HTTP" \
---exclude="Id.* should be .*ID" \
---exclude="Json.* should be .*JSON" \
---exclude="Url.* should be .*URL" \
---exclude="[iI][dD] can be fmt\.Stringer" \
---exclude=" duplicate of [A-Za-z\._0-9]*" \
-./... | tee $root/lint.txt
-wc -l $root/lint.txt
+#gometalinter \
+#--deadline=60s \
+#--concurrency=6 \
+#--vendor \
+#--exclude="exported (var)|(method)|(const)|(type)|(function) [A-Za-z\.0-9]* should have comment" \
+#--exclude="comment on exported function [A-Za-z\.0-9]* should be of the form" \
+#--exclude="Api.* should be .*API" \
+#--exclude="Http.* should be .*HTTP" \
+#--exclude="Id.* should be .*ID" \
+#--exclude="Json.* should be .*JSON" \
+#--exclude="Url.* should be .*URL" \
+#--exclude="[iI][dD] can be fmt\.Stringer" \
+#--exclude=" duplicate of [A-Za-z\._0-9]*" \
+#./... | tee $root/lint.txt
+#wc -l $root/lint.txt
 
 # gather some data about the repo
 
 cd $root
 tar cvzf $APP.$EXT \
-    geojson-go.cov \
-    lint.txt
+geojson-go.cov \
+geojson-go.cov.txt
+#    lint.txt
 tar tzf $APP.$EXT
 
